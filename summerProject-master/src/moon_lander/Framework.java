d81d42beea07683acb803b5c9bbe71900b6bcb9a
jinhui-lee <fkdkfk1578@gmail.com>
2022-07-20 14:45:59 +0900

    keyFramework modify

--- summerProject-master/src/moon_lander/Framework.java
+++ summerProject-master/src/moon_lander/Framework.java
@@ -565,14 +565,12 @@ public class Framework extends Canvas {
         switch (gameState)
         {
             case MAIN_MENU:
-                newGame(); //원래 있던 곳
                 break;
             case DESCRIPTION:
                 gameState = GameState.OPTIONS;
                 selectCharacter();
                 break;
             case OPTIONS:
-                newGame();
                 break;
             case GAMEOVER:
                 if(e.getKeyCode() == KeyEvent.VK_SPACE || e.getKeyCode() == KeyEvent.VK_ENTER)
