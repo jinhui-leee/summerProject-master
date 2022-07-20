94e2eb4806c0ba2486a1225c8778cebe80693468
jinhui-lee <fkdkfk1578@gmail.com>
2022-07-20 15:05:25 +0900

    Enemy insert

---
+++ summerProject-master/src/moon_lander/Enemy.java
@@ -0,0 +1,16 @@
+package moon_lander;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Enemy {
+    Image enemyImg = new ImageIcon("src/images/star.png").getImage();
+    int x, y;
+    int width = enemyImg.getWidth(null);
+    int height = enemyImg.getHeight(null);
+
+    public Enemy(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
